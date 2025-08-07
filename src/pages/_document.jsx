a8703fd3bfac2692d4f378a12c70671482c79128
Suvarna Mondal <suvarna.mondal@suvarna.local>
2025-08-07 01:24:29 +0100

    update the viewport settings

---
+++ src/pages/_document.jsx
@@ -0,0 +1,27 @@
+// src/pages/_document.tsx
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* ✅ Critical for responsive design */}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Merienda&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body className="font-sans">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
